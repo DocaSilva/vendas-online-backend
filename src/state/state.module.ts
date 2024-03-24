84a16e890e72d70eb3b758adeb6e25dce0d4ec9d
DocaSilva <rr3n4n@gmail.com>
2024-03-24 14:58:30 -0300

    Feat: create dto using pipes

--- src/state/state.module.ts
+++ src/state/state.module.ts
@@ -3,9 +3,10 @@ import { StateController } from './state.controller';
 import { StateService } from './state.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StateEntity } from './entities/state.entity';
+import { CacheModule } from 'src/cache/cache.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StateEntity])],
+  imports: [TypeOrmModule.forFeature([StateEntity]), CacheModule],
   controllers: [StateController],
   providers: [StateService],
 })
--- src/state/state.service.ts
+++ src/state/state.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { StateEntity } from './entities/state.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
 export class StateService {
   constructor(
     @InjectRepository(StateEntity)
     private readonly stateRepository: Repository<StateEntity>,
+    private readonly cahceService: CacheService,
   ) {}
 
   async getAllState(): Promise<StateEntity[]> {
-    return this.stateRepository.find();
+    return this.cahceService.getCache<StateEntity[]>('state_all', () =>
+      this.stateRepository.find({}),
+    );
   }
 }
