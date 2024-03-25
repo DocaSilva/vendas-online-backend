adaa0b4e172315c8f1579d3b727778b8d7b31489
DocaSilva <rr3n4n@gmail.com>
2024-03-25 17:24:21 -0300

    Feat: create token JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,20 @@ import { LoginDto } from './dtos/login.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { LoginPayloadDto } from './dtos/loginPayload.dto';
+import { ReturnLoginDto } from './dtos/returnLogin.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: LoginDto): Promise<UserEntity> {
-    const user: UserEntity = await this.userService
+  async login(loginDto: LoginDto): Promise<ReturnLoginDto> {
+    const user: UserEntity | undefined = await this.userService
       .findUserByEmail(loginDto.email)
       .catch(() => undefined);
 
@@ -19,6 +26,9 @@ export class AuthService {
       throw new NotFoundException('Email or password invalid');
     }
 
-    return user;
+    return {
+      accessToken: this.jwtService.sign({ ...new LoginPayloadDto(user) }),
+      user: new ReturnUserDto(user),
+    };
   }
 }
